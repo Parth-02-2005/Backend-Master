f7e7c6560d02853a210fd4450945f7db18fb6fb3
GANDHI PARTH MAHENDRA <gandhiparth.02.2005@gmail.com>
2025-09-27 17:51:47 +0530

    utils api error, response, middleware

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
 import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-}); 
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true,
+}));
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
+app.use(express.json({ limit: '10mb' })); // middleware to parse JSON bodies
+app.use(express.urlencoded({ extended: true, limit: '10mb' })); // middleware to parse URL-encoded bodies
+app.use(express.static('public')); // middleware to serve static files from the 'public' directory
+app.use(cookieParser()); // middleware to parse cookies
 export default app;
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,15 @@
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => { 
+        Promise.resolve(requestHandler(req, res, next)).catch(next);
+    };  
+}
+
+export default asyncHandler;
+ 
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(500).json({ success: false, message: error.message });
+//   }
+// }
