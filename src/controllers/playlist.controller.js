ce12adaa90f76dc60b7457f257050c5ad78d2897
GANDHI PARTH MAHENDRA <gandhiparth.02.2005@gmail.com>
2025-10-28 13:09:30 +0530

    Enhance playlist controller with addVideoToPlaylist functionality and update routes

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -1,100 +1,148 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import PlayList, {Playlist} from "../models/playlist.model.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
-import apiError from "../utils/apiError.js"
-import apiResponse from "../utils/apiResponse.js"
-import { User } from "../models/user.model.js"
-
+import mongoose, { isValidObjectId } from "mongoose";
+import PlayList, { Playlist } from "../models/playlist.model.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import apiError from "../utils/apiError.js";
+import apiResponse from "../utils/apiResponse.js";
+import { User } from "../models/user.model.js";
+import { Video } from "../models/video.model.js";
 
 const createPlaylist = asyncHandler(async (req, res) => {
-    const { name, description } = req.body
+  const { name, description } = req.body;
 
-    //TODO: create playlist
+  //TODO: create playlist
 
-    if ([name, description].some((field) => !field?.trim())) {
-        throw new apiError(400, 'Name or Description is required')
-    }
+  if ([name, description].some((field) => !field?.trim())) {
+    throw new apiError(400, "Name or Description is required");
+  }
 
-    const user = await User.findById(req.user._id);
+  const user = await User.findById(req.user._id);
 
-    if(!user) {
-        throw new apiError(404, 'user not found');
-    }
+  if (!user) {
+    throw new apiError(404, "user not found");
+  }
 
-    const playlist = await Playlist.create({
-        name,
-        description,
-        owner: req.user._id,
-        video: []
-    })
+  const playlist = await Playlist.create({
+    name,
+    description,
+    owner: req.user._id,
+    video: [],
+  });
 
-    res.status(200).json(new apiResponse(200, playlist, 'playlist created successfully'));
-
-})
+  res
+    .status(200)
+    .json(new apiResponse(200, playlist, "playlist created successfully"));
+});
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
-    const { userId } = req.params
-    //TODO: get user playlists
-    if(!userId || !userId.trim()) {
-        throw new apiError(404, 'User Id not found');
-    }
-    const user = await User.findById(userId);
+  const { userId } = req.params;
+  //TODO: get user playlists
+  if (!userId || !userId.trim()) {
+    throw new apiError(404, "User Id not found");
+  }
+  const user = await User.findById(userId);
 
-    if(!user) {
-        throw new apiError(400, 'Invalid user');
-    }
+  if (!user) {
+    throw new apiError(400, "Invalid user");
+  }
 
-    const playlist = await Playlist.find({
-        owner: userId
-    }).populate('videos')
+  const playlist = await Playlist.find({
+    owner: userId,
+  }).populate("videos");
 
-    res.status(200).json(new apiResponse(200, playlist, 'fetched user playlist'))
-
-
-})
+  res.status(200).json(new apiResponse(200, playlist, "fetched user playlist"));
+});
 
 const getPlaylistById = asyncHandler(async (req, res) => {
-    const {playlistId} = req.params
-    //TODO: get playlist by id
-    if(!playlistId || !playlistId.trim()) {
-        throw new apiError(400, 'Invalid playlist ID');
-    }
-    const playlist = await PlayList.findById(playlistId).populate('videos').populate('owner', 'username email');
+  const { playlistId } = req.params;
+  //TODO: get playlist by id
+  if (!playlistId || !playlistId.trim()) {
+    throw new apiError(400, "Invalid playlist ID");
+  }
+  const playlist = await PlayList.findById(playlistId)
+    .populate("videos")
+    .populate("owner", "username email");
 
-    if(!playlist) {
-        throw new apiError(404, 'playlist not found');
-    }
+  if (!playlist) {
+    throw new apiError(404, "playlist not found");
+  }
 
-    res.status(200).json(new apiResponse(200, playlist, 'Fetched playlist successfully'));
-})
+  res
+    .status(200)
+    .json(new apiResponse(200, playlist, "Fetched playlist successfully"));
+});
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
-    const {playlistId, videoId} = req.params
-})
+  const { playlistId, videoId } = req.params;
+
+  if (!playlistId || !videoId) {
+    throw new apiError(400, "Playlist ID and Video ID are required");
+  }
+  // find video in database
+  const video = await Video.findById(videoId);
+  const playlist = await PlayList.findById(playlistId);
+
+  if (!playlist) {
+    throw new apiError(404, "Playlist not found");
+  }
+
+  if (!video) {
+    throw new apiError(404, "Video not found");
+  }
+  // check if video already exists in playlist or not
+  const videoInPlaylist = playlist.videos.some(
+    (video) => video.toString() === videoId
+  );
+
+  if (videoInPlaylist) {
+    throw new apiError(400, "video already in playlist");
+  }
+  // push the id of video in playlist
+  playlist.videos.push(videoId);
+  const updatePlaylist = await playlist.save();
+
+  if(!updatePlaylist) {
+    throw new apiError(404, 'Failed to update playlist');
+  }
+
+  const populatePlaylist = await updatePlaylist.populate({
+    path: "videos",
+    select: "title thumbnail duration createdAt",
+  }).populate({
+    path: "owner",
+    select: "username email"
+  });
+
+  if(!populatePlaylist)  {
+    throw new apiError(404, "playlist not found");
+  }
+
+  res
+    .status(200)
+    .json(new apiResponse(200, populatePlaylist, 'Video added to playlist successfully'));
+});
 
 const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
-    const { playlistId, videoId } = req.params
-    // TODO: remove video from playlist
-
-})
+  const { playlistId, videoId } = req.params;
+  // TODO: remove video from playlist
+});
 
 const deletePlaylist = asyncHandler(async (req, res) => {
-    const {playlistId} = req.params
-    // TODO: delete playlist
-})
+  const { playlistId } = req.params;
+  // TODO: delete playlist
+});
 
 const updatePlaylist = asyncHandler(async (req, res) => {
-    const {playlistId} = req.params
-    const {name, description} = req.body
-    //TODO: update playlist
-})
+  const { playlistId } = req.params;
+  const { name, description } = req.body;
+  //TODO: update playlist
+});
 
 export {
-    createPlaylist,
-    getUserPlaylists,
-    getPlaylistById,
-    addVideoToPlaylist,
-    removeVideoFromPlaylist,
-    deletePlaylist,
-    updatePlaylist
-}
+  createPlaylist,
+  getUserPlaylists,
+  getPlaylistById,
+  addVideoToPlaylist,
+  removeVideoFromPlaylist,
+  deletePlaylist,
+  updatePlaylist,
+};
--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,7 +1,7 @@
 import express from "express"
 import upload from "../middlewares/multer.middleware.js";
 import { authenticateUser } from "../middlewares/auth.middleware.js";
-import { createPlaylist, getPlaylistById, getUserPlaylists } from "../controllers/playlist.controller";
+import { addVideoToPlaylist, createPlaylist, getPlaylistById, getUserPlaylists } from "../controllers/playlist.controller";
 
 
 const router = express.Router()
@@ -9,3 +9,8 @@ const router = express.Router()
 router.post('/playlist', authenticateUser, createPlaylist);
 router.get('/playlists/:userId', authenticateUser, getUserPlaylists);
 router.get('/playlist/:playlistId', authenticateUser, getPlaylistById);
+router.post(
+  '/playlist/:playlistId/add-video/:videoId',
+  authenticateUser,
+  addVideoToPlaylist
+);
